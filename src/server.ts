0552fd4334da0cf7821a767d3e67b7ee85614055
mybox1407-art <mybox1407@gmail.com>
2026-07-17 13:17:37 +0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 
-const port = Number(process.env.PORT) || 3001;
+const port = Number(process.env.PORT) || 3011;
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server started on port ${port}`);
