51a84a624f6a1d1428057da40698a5a99e688f97
mybox1407-art <mybox1407@gmail.com>
2026-07-17 13:35:22 +0400

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
-  port: Number(process.env.PORT) || 3001,
+  port: Number(process.env.PORT) || 3011,
   tinkoffToken: process.env.TINKOFF_TOKEN || '',
   tinkoffSandbox: (process.env.TINKOFF_SANDBOX ?? 'true') === 'true'
 };
