db6e8b6eba3ca37f7d802914dec65c817e83a32e
mybox1407-art <mybox1407@gmail.com>
2026-07-19 14:38:20 +0400

    Update runBacktest.ts

--- trade_bot_tbank/src/backtest/runBacktest.ts
+++ trade_bot_tbank/src/backtest/runBacktest.ts
@@ -7,7 +7,7 @@ import { Candle } from '../services/strategy';
  * Количество свечей паузы после убыточной сделки / стопа.
  * Можно менять через CLI четвертым аргументом.
  */
-const DEFAULT_COOLDOWN_CANDLES = 4;
+const DEFAULT_COOLDOWN_CANDLES = 8;
 
 /**
  * Как часто печатать прогресс в консоль.
